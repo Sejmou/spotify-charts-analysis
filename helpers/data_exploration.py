773e7e0e55f25979b7230f7058e8057482cfc6d7
Sejmou <samo.kolter@gmail.com>
2023-06-26 14:59:41 +0200

    add some helper functions for data exploration

---
+++ helpers/data_exploration.py
@@ -0,0 +1,54 @@
+import json
+from contextlib import redirect_stdout
+from .data import create_data_path
+import pandas as pd
+import os
+
+
+def pretty_print_val(val):
+    if isinstance(val, dict) or isinstance(val, list):
+        return json.dumps(val, indent=4)
+    return val
+
+
+def write_dict_to_file_as_prettified_json(
+    dictionary: dict, file_path=create_data_path("pretty_out.json")
+):
+    with open(file_path, "w") as f:
+        with redirect_stdout(f):
+            print(json.dumps(dictionary, indent=4))
+
+
+def write_series_to_file_as_prettified_json(
+    series: pd.Series, file_path=create_data_path("pretty_out.json")
+):
+    with open(file_path, "w") as f:
+        with redirect_stdout(f):
+            print(series.to_json(indent=4))
+
+
+def load_parquet_files_in_dir(dir_path: str):
+    """
+    Load all .parquet files in a directory into a dictionary of DataFrames.
+
+    Args:
+        dir_path (str): Path to directory containing .parquet files.
+
+    Returns:
+        dict: Dictionary of DataFrames, where the keys are the file names (without the .parquet extension).
+
+    """
+
+    df_dict = {}
+    files = [
+        f
+        for f in os.listdir(dir_path)
+        if os.path.isfile(os.path.join(dir_path, f)) and f.endswith(".parquet")
+    ]
+
+    for file in files:
+        file_path = os.path.join(dir_path, file)
+        df = pd.read_parquet(file_path)
+        df_dict[file.split(".")[0]] = df
+
+    return df_dict
